15823efaa9850e6e6b7cbf1210c57ad786e9adbe
melmarie <146302791+melmarie1999@users.noreply.github.com>
2023-09-28 10:26:20 +0800

    Add files via upload

---
+++ practice 7.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std ;
+
+int main(){
+	
+	string x = "HELLO WORLD!" ;
+	int x_size = x.size();
+	x [0] = 'j';
+	x_size -= 4;
+	cout<<x <<"/n ";
+	cout<<x_size <<"/n";
+	cout << x[x_size] <<"/n";
+	
+	
+	
+	
+	
+}
