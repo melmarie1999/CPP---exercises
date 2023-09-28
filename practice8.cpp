2ee0c072b171061f5d6bbc93f50693b3f8d4b0a9
melmarie <146302791+melmarie1999@users.noreply.github.com>
2023-09-28 10:57:07 +0800

    Add files via upload

---
+++ practice8.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	
+	string myname;
+	
+	cout<<"enter your name:";
+	getline(cin,myname);
+
+ 	cout<<"the length of your name is:"<<myname.size();
+	
+	
+	
+	
+	
+}
