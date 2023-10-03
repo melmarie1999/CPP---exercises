86b5476ed741cb9386523fc52c11d8cfd21b7a4b
melmarie <146302791+melmarie1999@users.noreply.github.com>
2023-10-03 10:23:34 +0800

    Add files via upload

---
+++ practice9mel.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+	
+	int x = -100, y = 90, z = 50;
+	x = min(x, y);
+	x = abs(x);
+	x =sqrt(x);	
+	cout<<x;	
+	
+	
+	
+	return 0;
+	
+	
+}
