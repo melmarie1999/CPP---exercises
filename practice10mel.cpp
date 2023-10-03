3a22318d6c85c90f7138af230e0db4b8e2779c89
melmarie <146302791+melmarie1999@users.noreply.github.com>
2023-10-03 10:24:10 +0800

    Add files via upload

---
+++ practice10mel.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	int grade;
+	
+	cout<<"Enter your grade: ";
+	cin>>grade;
+
+	if(grade >=75 ) {
+		cout<<"pasado";
+		
+		
+}	else {
+	cout<<"bagsak!";
+
+	
+	
+}
+return 0;	
+} 
