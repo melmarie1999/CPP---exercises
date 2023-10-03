7292aee49fd4591ccf4a15a24d6b6760f531af00
melmarie <146302791+melmarie1999@users.noreply.github.com>
2023-10-03 10:37:32 +0800

    Add files via upload

---
+++ practice12mel.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	int num1;
+	
+	cout<<"Enter a number:";
+	cin>>num1;
+
+	if(num1==0 ) {
+		cout<<"even number";
+		
+	}else {
+	cout<<"odd number";
+
+	
+	
+}
+return 0;	
+} 
